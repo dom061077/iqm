c06f17d54fdd55a453ce222f6d95d74feea71931
daniel <daniel@192.168.56.1>
2016-09-29 23:13:04 -0300

    templates config

--- www/views/procedimientos/procedimientodetailctrl.js
+++ www/views/procedimientos/procedimientodetailctrl.js
@@ -1,6 +1,7 @@
 angular.module('App')
-.controller('ProcedimientoDetailCtrl',function($scope, $stateParams, Procedimientos) {
+.controller('ProcedimientoDetailCtrl',function($scope, $stateParams,$sce, Procedimientos) {
     //$scope.procedimientoUrl = '';
     //$scope.url=$stateParams.urlVideo;
     $scope.procedimiento = Procedimientos.get($stateParams.procId);
+    $scope.procedimiento.urlVideo = $sce.trustAsResourceUrl($scope.procedimiento.urlVideo);
 });
