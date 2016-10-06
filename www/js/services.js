2091a1b31b994c4968b2ad97dbbb4825eca8ee58
daniel <daniel@192.168.56.1>
2016-10-05 22:40:05 -0300

    templates config

--- www/js/services.js
+++ www/js/services.js
@@ -1,14 +1,19 @@
 angular.module('starter.services', [])
-.factory('Procedimientos', function($http){
+.factory('Procedimientos', function($http,$ionicLoading){
   // Might use a resource here that returns a JSON array
   procedimientos = [];
 
-    
+    $ionicLoading.show();
     $http.get('http://200.58.126.188:8080/cima/mobileContenido/procedimientos').success(function (response) {
       angular.forEach(response, function (procedimiento) {
         procedimientos.push(procedimiento);
       });
+      $ionicLoading.hide();
     }).error(function (err) {
+        $ionicLoading.show({
+          template: 'No se puede traer el contenido',
+          duration: 3000
+        });
       console.log(err);
     });
   
